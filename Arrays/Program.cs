44d3010b7cc15ca7434fae31598e9aa022da4d7c
Ottomann <ott.ottender@gmail.com>
2023-11-19 19:25:25 +0200

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+//string[] fruit = { "apples", "bananas", "oranges" }; //ainult string, booleani ei saa prgu
+string[] fruit = new string[3]; //range suurusega jne fruit[0], fruit [1], fruit [2] ja nendesse saab salvestada andmeid
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+Console.WriteLine($"{fruit[0]}"); //jne
+
+int fruitArrayLength = fruit.Length;
+
+Console.WriteLine($"There are {fruitArrayLength} in your array.");
+
+for (int i = 0; i < fruitArrayLength; i++)
+{
+    //Console.WriteLine(fruit[i]);
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0])); //ei saa teha nt foreachis
+}
+
+foreach (string temporaryElement in fruit)
+{
+    Console.WriteLine(temporaryElement);
+} //v2imaldab displayida aga mitte sisuliselt muuta neid andmeid failis
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿//hello 5 t2hte
+//01234 asukoha index alati t2henumber - 1 //length - 1
+
+//string hello = "hello, world! Here I am.";
+string hello = "we are the champions?";
+
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim(); //kustutab tuhiku enne ja parast sona
+
+hello = hello.Trim(); //kustutaks varasem muutuja ja asendatakse see, et v2hendada vahe malu suurust
+
+char firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace(hello[20], '!'); //voi hello[20] asemel '?'
+
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+//string andmed immutable
+
+//int wordCounter = 1;
+////Console.WriteLine($"{i} symbol in tring {hello[i]}");
+//for (int i = 0; i < hello.Length; i++) 
+//{
+//    // if(hello[i] == ' ') 
+//    //{
+//    //  wordCounter++;
+//    //}
+//    //if (char.IsWhiteSpace(hello[i])) //syboliga false tuhikuga true
+//    //{
+//    //    wordCounter++;
+//    //}  
+//}
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
